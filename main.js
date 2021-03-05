8f41887078c1ca6d90ff7008f484200030890f56
Noah MacMurchy <36996119+njmacmurchy96@users.noreply.github.com>
2021-03-05 12:16:44 -0600

    Fixed bug where no elements found
    Jobber updated element ids and classes. Changed to search by class, check if null or empty, and then continue.

--- main.js
+++ main.js
@@ -7,19 +7,21 @@
 // @match        https://secure.getjobber.com/time_sheet/*/*/*/week
 // @grant        none
 // @require      http://code.jquery.com/jquery-latest.js
-// @copyright    2020+, Noah MacMurchy
+// @copyright    2021+, Noah MacMurchy
 // ==/UserScript==
 var $ = window.jQuery;
 
 $(document).ready(function() {
     var totalHours = 0.0;
-    $(".u-textSmall.u-paddingTopSmallest.u-paddingBottomSmallest.js-weeklyTimeSheetTotalTimeNumber").each(function(i) {
-        var [hours, minutes] = this.innerText.split(":");
+    $('.duration.placeholderField-input').each(function(i) {
+        if (this.value !== "" || this.value !== " " || this.value !== "0:00")
+            return;
+        var [hours, minutes] = this.value.split(":");
         hours = parseInt(hours);
         minutes = parseInt(minutes);
         totalHours += hours + minutes / 60;
     });
-    $(".headingFive.u-marginNone.u-colorGreyBlueDark")[0].innerText = "Total Hours: " + convertToTime(totalHours);
+    $(".headingFive.u-marginNone.u-colorGreyBlueDark")[0].innerText = "Total: " + convertToTime(totalHours);
 });
 
 function convertToTime(number) {
